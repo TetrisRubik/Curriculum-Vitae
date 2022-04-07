22ed25e4424f323b4987ad66f5c5d46300d67fde
TetrisRubik <fpablo960@gmail.com>
2022-04-07 15:36:28 +0100

    Tema Refactorizado Ahora las rutas claro/oscuro están puestas en variables.

--- funciones/tema.js
+++ funciones/tema.js
@@ -1,10 +1,13 @@
 //* Función que cambia el tema de oscuro a claro y viceversa.
 export default function cambiar_tema(nodo, accionador) {
+	const oscuro = "estilos/oscuro.css";
+	const claro = "estilos/claro.css";
+
 	accionador.addEventListener("click", () => {
-		if (nodo.getAttribute("href") === "estilos/oscuro.css") {
-			nodo.setAttribute("href", "estilos/claro.css");
+		if (nodo.getAttribute("href") === oscuro) {
+			nodo.setAttribute("href", claro);
 		} else {
-			nodo.setAttribute("href", "estilos/oscuro.css");
+			nodo.setAttribute("href", oscuro);
 		}
 	});
 }
