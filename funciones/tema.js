2081148644655d4a3c81f0b880873367a23194b0
TetrisRubik <fpablo960@gmail.com>
2022-04-10 01:58:56 +0100

    Icono Temas Ahora el botón cambia tema es una imagen vectorial que cambia en función del tema actual.

--- funciones/tema.js
+++ funciones/tema.js
@@ -1,13 +1,21 @@
 //* Función que cambia el tema de oscuro a claro y viceversa.
 export default function cambiar_tema(nodo, accionador) {
-	const oscuro = "estilos/oscuro.css";
-	const claro = "estilos/claro.css";
+	const tema_oscuro = "estilos/oscuro.css";
+	const tema_claro = "estilos/claro.css";
+	const icono_oscuro = "archivos/modo_oscuro.svg";
+	const icono_claro = "archivos/modo_claro.svg";
+	const alternativa_oscuro = "Modo Oscuro";
+	const alternativa_claro = "Modo Claro";
 
 	accionador.addEventListener("click", () => {
-		if (nodo.getAttribute("href") === oscuro) {
-			nodo.setAttribute("href", claro);
+		if (nodo.getAttribute("href") === tema_oscuro) {
+			nodo.setAttribute("href", tema_claro);
+			accionador.setAttribute("src", icono_oscuro);
+			accionador.setAttribute("alt", alternativa_oscuro);
 		} else {
-			nodo.setAttribute("href", oscuro);
+			nodo.setAttribute("href", tema_oscuro);
+			accionador.setAttribute("src", icono_claro);
+			accionador.setAttribute("alt", alternativa_claro);
 		}
 	});
 }
