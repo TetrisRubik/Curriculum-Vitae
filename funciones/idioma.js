95c1498062d1d09c4c2a0b83137be3e21c6e406a
TetrisRubik <fpablo960@gmail.com>
2022-04-14 02:50:14 +0100

    Cambio de Idioma La opción de cambiar idioma está implementada. Tanto la información, proyectos e idiomas se modifican en función del idioma. Se tiene pensado que afecte el idioma en casi todos los campos de la página.

---
+++ funciones/idioma.js
@@ -0,0 +1,45 @@
+//* Función que cambia el idioma de la página.
+import carga_json from "./json.js";
+import añadir_proyectos from "./proyectos.js";
+import añadir_información from "./información.js";
+import añadir_idiomas from "./idiomas.js";
+
+export default function cambiar_idioma(idioma) {
+	const nodo_información = document.getElementById("información");
+	const nodo_proyectos = document.getElementById("proyectos");
+	const nodo_idiomas = document.getElementById("idiomas");
+	const datos = carga_json("archivos/objetos/" + idioma + ".json");
+	const lang = {
+		"deutsch": "de",
+		"english": "en",
+		"español": "es",
+		"français": "fr",
+		"italiano": "it",
+		"português": "pt",
+		"中文": "zh",
+		"日本語": "ja",
+		"한국어": "ko"
+	};
+
+	// Ponemos la página web en el idioma correspondiente.
+	document.querySelector("html").setAttribute("lang", lang[idioma]);
+
+	datos.then(datos => {
+		// Tratar información del usuario.
+		borrar_hijos(nodo_información);
+		añadir_información(nodo_información, datos.información);
+		// Tratar proyectos realizados.
+		borrar_hijos(nodo_proyectos);
+		añadir_proyectos(nodo_proyectos, datos.proyectos);
+		// Tratar listado de idiomas.
+		borrar_hijos(nodo_idiomas);
+		añadir_idiomas(nodo_idiomas, datos.idiomas);
+	});
+}
+
+function borrar_hijos(nodo) {
+	// Borramos los nodos hijos si hay.
+	while (nodo.firstChild) {
+		nodo.removeChild(nodo.firstChild);
+	}
+}
--- funciones/principal.js
+++ funciones/principal.js
@@ -1,42 +1,21 @@
 //* Guión principal.
-import añadir_lenguajes from "./lenguajes.js";
-import cambiar_tema from "./tema.js";
 import carga_json from "./json.js";
-import añadir_proyectos from "./proyectos.js";
-import añadir_información from "./información.js";
-import añadir_idiomas from "./idiomas.js";
+import cambiar_idioma from "./idioma.js";
+import cambiar_tema from "./tema.js";
+import añadir_lenguajes from "./lenguajes.js";
 
 const botón_idioma = document.getElementById("botón_idioma");
 const botón_tema = document.getElementById("botón_tema");
 const nodo_estilo = document.getElementById("tema");
 const nodo_lenguajes = document.getElementById("lenguajes");
-const nodo_proyectos = document.getElementById("proyectos");
-const nodo_información = document.getElementById("información");
-const nodo_idiomas = document.getElementById("idiomas");
-const datos = carga_json("archivos/objetos/español.json");
 
-//# Añadir proyectos (esto estará dentro de cambiar_idioma).
-datos.then(datos => {
-	añadir_proyectos(nodo_proyectos, datos.proyectos);
-});
-
-//# Añadir información (esto estará dentro de cambiar_idioma).
-datos.then(datos => {
-	añadir_información(nodo_información, datos.información)
-})
-
-//# Añadir idiomas (esto estará dentro de cambiar_idioma).
-datos.then(datos => {
-	añadir_idiomas(nodo_idiomas, datos.idiomas)
-})
-
-// Cargamos los valores por defecto desde el json variables.
+// Cargamos los valores por defecto desde el archivo variables.
 const jsonURL = "archivos/objetos/variables.json";
 const predeterminado = carga_json(jsonURL);
 
 predeterminado.then(datos => {
 	// Ponemos la web a el idioma por defecto.
-	//# cambiar_idioma(datos.idioma);
+	cambiar_idioma(datos.idioma);
 	// Cambiamos al tema por defecto.
 	if (datos.modo_oscuro) cambiar_tema(nodo_estilo, botón_tema);
 	// Coloca los lenguajes aprendidos y su nivel.
@@ -45,10 +24,10 @@ predeterminado.then(datos => {
 
 // Botón para cambiar el idioma.
 botón_idioma.addEventListener("change", () => {
-	//# cambiar_idioma(botón_idioma.value);
+	cambiar_idioma(botón_idioma.value);
 });
 
-// Botón para cambiar tema claro/oscuro.
+// Botón para cambiar el tema.
 botón_tema.addEventListener("click", () => {
 	cambiar_tema(nodo_estilo, botón_tema);
 });
