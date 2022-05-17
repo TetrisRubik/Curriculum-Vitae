4db2a8a7c7ad94b7e7ee6f2a104aa8983db70fc3
TetrisRubik <fpablo960@gmail.com>
2022-05-17 01:29:33 +0100

    Información Adicional en Lenguajes Ahora al pasar el ratón por encima de algún lenguaje de programación se mostrará información relativa al nivel de este.

--- funciones/contenido/lenguajes.js
+++ funciones/contenido/lenguajes.js
@@ -9,16 +9,11 @@ export default function añadir_lenguajes(nodo, lenguajes) {
 
 	// Variables de la lista.
 	const nodo_lista = document.createElement("UL");
-	const atributo = "class";
-	const marcador = "▣" // Posibles marcadores para cada color: ▩▨▧▦▥▤▣▢.
-	const color = {
-		5: "azul",
-		4: "verde",
-		3: "amarillo",
-		2: "naranja",
-		1: "rojo",
-		0: "negro"
-	}
+	const atributo_color = "class";
+	const atributo_descripción = "title";
+	const marcador = "▣"; // Posibles marcadores para cada color: ▩▨▧▦▥▤▣▢.
+	const color = { 5: "azul", 4: "verde", 3: "amarillo", 2: "naranja", 1: "rojo" };
+	const defecto = "negro";
 
 	// Transforma cada dato a elemento de lista.
 	for (const lenguaje of lenguajes.elementos) {
@@ -27,8 +22,9 @@ export default function añadir_lenguajes(nodo, lenguajes) {
 		// Nivel de experiencia en el lenguaje.
 		const nivel = document.createElement("SPAN");
 		nivel.textContent = marcador;
-		nivel.setAttribute(atributo, color[lenguaje.nivel])
+		nivel.setAttribute(atributo_color, color[lenguaje.nivel] ? color[lenguaje.nivel] : defecto);
 		elemento.appendChild(nivel);
+		elemento.setAttribute(atributo_descripción, lenguajes.descripción[lenguaje.nivel] ? lenguajes.descripción[lenguaje.nivel] : lenguajes.descripción[0]);
 
 		// Nombre del lenguaje.
 		const nombre = document.createElement("SPAN");
